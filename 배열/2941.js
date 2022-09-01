2617a573e6cf15586f4e9b1728e281d69b75073f
Innovanito <khg05311@naver.com>
2022-09-01 15:21:47 +0900

    220901

--- 배열/2941.js
+++ 배열/2941.js
@@ -1,3 +1,7 @@
 // const input=fs.readFileSync('/dev/stdin').toString();
 
 const input = require('fs').readFileSync("../input.txt").toString();
+
+let regExp = /c\=|c\-|dz\=|d\-|lj|nj|s\=|z\=/g;
+let result = input.replace(regExp, '-')
+console.log(result);
