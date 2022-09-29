9e729cde60335ba1e7cde08d1b2ca1214441aa2c
Innovanito <khg05311@naver.com>
2022-09-30 00:14:25 +0900

    220930 2nd

--- 집합과맵/14425.js
+++ 집합과맵/14425.js
@@ -2,4 +2,20 @@ let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let input = fs.readFileSync('../input.txt').toString().trim().split('\n')
 
-console.log(input);
+
+const a = input[0].split(' ').map(x => Number(x))
+
+const N = a[0]
+
+const M = a[1]
+
+let cnt = 0
+
+
+for (let i = 1; i <= N; i++) {
+  for (let j = N + 1; j <= M + N; j++) {
+    if(input[i] === input[j]) cnt++
+  }
+}
+
+console.log(cnt);
