62932202068bd05f5c3963ca4f337a872aceb03b
Innovanito <khg05311@naver.com>
2023-03-12 22:38:49 +0900

    230312

--- 동적계획법/11726.py
+++ 동적계획법/11726.py
@@ -14,6 +14,10 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-
 n = int(input())
+rs = [0, 1, 2]
+
+for i in range(3, n+1):
+    rs.append((rs[i-1] + rs[i-2]) % 10007)
+
+print(rs[n])
